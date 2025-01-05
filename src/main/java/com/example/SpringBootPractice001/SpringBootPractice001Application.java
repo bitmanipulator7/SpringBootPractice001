fb9b97cb550706048d18d227fbb6e755142acc5f
Paranjyoti Mohanty <paranjyoti.mohanty@gmail.com>
2025-01-05 23:50:11 +0530

    Initial commit

---
+++ src/main/java/com/example/SpringBootPractice001/SpringBootPractice001Application.java
@@ -0,0 +1,11 @@
+package com.example.SpringBootPractice001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPractice001Application {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPractice001Application.class, args);
+	}
+}
